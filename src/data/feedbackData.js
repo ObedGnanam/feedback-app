9b3d1cbeab76d5681667d5b13ec65f4de78376a4
ObedGnanam <123256725+ObedGnanam@users.noreply.github.com>
2023-05-19 11:59:50 +0530

    Add files via upload

---
+++ src/data/feedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id:1,
+        rating:10,
+        text: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    },
+    {
+        id:2,
+        rating:9,
+        text: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    },
+    {
+        id:3,
+        rating:8,
+        text: 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum'
+    }
+]
+
+export default FeedbackData
